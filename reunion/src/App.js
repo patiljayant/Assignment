166fae322cc6094dc64873165ea531c5a1050666
paitljayant <contactjaypatil@gmail.com>
2022-12-21 13:33:08 +0530

    changes applied

--- reunion/src/App.js
+++ reunion/src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Rent from './pages/rent/Rent';
 
 
 function App() {
   return (
-    
-    <Router>
       <div className='bg-[#f8f7fd]'>
         <Navbar />
-          <Routes>
-            <Route path={'/rent'} element={<Rent /> } />
-          </Routes>
+        <Rent />
       </div>
-    </Router>
   );
 }
 
